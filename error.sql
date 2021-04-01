f8632290e660113d8f85de39db96bc78cc55dd54
jas <jasmine-lim88@hotmail.com>
2021-04-01 23:10:52 +0800

    error database

---
+++ error.sql
@@ -0,0 +1,12 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE DATABASE IF NOT EXISTS `error` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `error`;
+
+CREATE TABLE `error` (
+  `error_id` int(11) NOT NULL AUTO_INCREMENT,
+  `error_category` varchar(64) NULL,
+  `error_desc` varchar(64) NOT NULL,
+  PRIMARY KEY (`error_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
