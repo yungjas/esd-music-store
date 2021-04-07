eade0d4271f70339353af266a38302de9e2a07c0
Shawntwj <shawntwj20@gmail.com>
2021-04-07 23:23:33 +0800

    docker update

--- place_order.py
+++ place_order.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 import os
+from os import environ
 
 import requests
 from invokes import invoke_http
@@ -10,7 +11,7 @@ import amqp_setup
 import pika
 import json
 
-from os import environ
+
 
 app = Flask(__name__)
 CORS(app)
